cae178ef2553cf4ffe5646e792b2e08feb47b3b1
Junyu Long <877730493@qq.com>
2024-08-05 17:06:09 +0800

    Bump version from v7.1 to v7.1.1

--- app/src/main/java/com/winlator/xenvironment/ImageFsInstaller.java
+++ app/src/main/java/com/winlator/xenvironment/ImageFsInstaller.java
@@ -26,7 +26,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
 public abstract class ImageFsInstaller {
-    public static final byte LATEST_VERSION = 8;
+    public static final byte LATEST_VERSION = 9;
 
     private static void resetContainerImgVersions(Context context) {
         ContainerManager manager = new ContainerManager(context);
