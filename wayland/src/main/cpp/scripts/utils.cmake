9b3bff86d518c03bce00bd70b7c4f1ee744f1ded
Junyu Long <877730493@qq.com>
2024-09-15 11:08:45 +0800

    libffi: Update CMakeLists.txt.

--- wayland/src/main/cpp/scripts/utils.cmake
+++ wayland/src/main/cpp/scripts/utils.cmake
@@ -4,3 +4,14 @@ function(check_repo path)
         message(FATAL_ERROR "Repository is empty, please execute 'git submodule update' before building.")
     endif ()
 endfunction()
+
+function(aarch64_only)
+    check_c_source_compiles("
+    #ifndef __aarch64__
+        #error Support aarch64 only.
+    #endif
+" IS_AARCH64)
+    if (NOT DEFINED IS_AARCH64)
+        message(FATAL_ERROR "Support aarch64 only.")
+    endif ()
+endfunction()
