3f1b3d5f556280b61f81d6a24994b2ffea5badd5
Lubos <tridosm@gmail.com>
2024-09-30 12:02:11 +0200

    Turnip 24.3.0-R7v2 added

--- app/src/main/java/com/winlator/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/core/DefaultVersion.java
@@ -3,7 +3,7 @@ package com.winlator.core;
 public abstract class DefaultVersion {
     public static final String BOX86 = "0.3.2";
     public static final String BOX64 = "0.3.0";
-    public static final String TURNIP = "24.1.0";
+    public static final String TURNIP = "24.3.0-R7v2";
     public static final String ZINK = "22.2.5";
     public static final String VIRGL = "23.1.9";
     public static final String DXVK = "2.3.1";
